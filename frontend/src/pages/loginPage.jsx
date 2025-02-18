316a902a9e438685d560960e8ce6ca2b7197c07c
L-Cherron <e0958318@u.nus.edu>
2025-02-18 11:10:15 +0800

    redecorated login page, added asset for bg img

--- frontend/src/pages/loginPage.jsx
+++ frontend/src/pages/loginPage.jsx
@@ -1,6 +1,9 @@
 import { useForm } from "react-hook-form";
 import "./loginPage.css";
 import { Outlet } from "react-router";
+import { useNavigate } from "react-router-dom"
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 /*
 Div centered on the login page
@@ -8,23 +11,56 @@ Div centered on the login page
 export default function LoginCredentialsDiv()
 {
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data    => console.log(data);
+    const navigate = useNavigate();
+    const location = useLocation();
+
+
+    // Placeholder check
+    const onSubmit = data => {
+        console.log(data);
+        if (data.email === "superadmin@nus") {
+            navigate("/home");
+        }
+    }
+
+
+    // Check if login to display image at backgound
+    useEffect(() => {
+        // Check if at login
+        if (location.pathname === '/') {
+            document.body.classList.add('login-page');
+        } else {
+            document.body.classList.remove('login-page');
+        }
+
+        // Cleanup
+        return () => {
+            document.body.classList.remove('login-page');
+        };
+    }, [location.pathname]);
+
     return (
-        <div className="
-            absolute top-1/2 left-1/2 -translate-1/2
-            border-gray-200 border-2
-            h-2/5 w-3/10
-            flex justify-center items-center">
-            <form className="flex flex-col justify-evenly items-center
-                            w-full h-full border-2 border-black"
-                            onSubmit={handleSubmit(onSubmit)}>
-                <input type="email" {...register("email", { required: true })} />
-                {errors.email && <span style={{ color: "red" }}>
-                    *Email* is mandatory </span>}
-                <input type="password" {...register("password")} />
-                <input type={"submit"} style={{ backgroundColor: "#a1eafb" }} />
+        <div className="login-container">
+
+            <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+                <label className="login-header">ESG Report</label>
+
+                {/* Label + Error message */}
+                <div className="input-container">
+                    <label className="field-label" htmlFor="email">Email</label>
+                    {errors.email && <span style={{ color: "red" }}> *Email* is mandatory </span>}
+                </div>
+                <input className="input-field" type="email" {...register("email", { required: true })} />
+
+
+                {/* Label + (To do) Error message */}
+                <div className="input-container">
+                    <label className="field-label" htmlFor="password">Password</label>
+                </div>
+                <input className="input-field" type="password" {...register("password")} />
+                <input className="form-button" type={"submit"} />
             </form>
-            <Outlet/ >
+            <Outlet />
         </div>
     )
 };

8253c096a663872a0116cb3749c51e01eef68a5b
OweT1 <owentan@hotmail.sg>
2025-02-21 01:22:12 +0800

    Added footer component for webpage

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; AY2024/25 DSA3101 Data Science in Practice Group 6</p>
+        <a
+          href="https://github.com/zzhenjie01/dsa3101-ay2425s2-team6"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View Project on GitHub
+        </a>
+      </div>
+    </footer>
+  );
+  d;
+}
+
+export default Footer;

633697c13b047da3609ae78206ace1003abc5832
OweT1 <owentan@hotmail.sg>
2025-02-21 01:27:41 +0800

    sidePanel updated for better code readability using LinkButton component

--- frontend/src/components/sidePanel.jsx
+++ frontend/src/components/sidePanel.jsx
@@ -1,39 +1,32 @@
-import './sidePanel.css';
-import { useContext } from 'react';
-import { SidePanelContext } from '../context/contexts';
+import "./SidePanel.css";
+import { useContext } from "react";
+import LinkButton from "./LinkButton";
+import { SidePanelContext } from "../context/contexts";
+// import LinkButton from "./LinkButton";
 
-export function SidePanelOptions({buttonLst})
-{
+export function SidePanelOptions({ buttonLst }) {
+  // const navigate = useNavigate();
   return (
     <div>
-    {buttonLst.map(btn =>
-      <button key={btn.idx} 
-              onClick={() => alert(btn.onClickText)}
-              className="
-              p-6
-              h-20 w-full
-              font-sans font-semibold font-size
-              text-left align-middle text-white
-              hover:bg-emerald-500
-              ">
-        {btn.buttonName}
-      </button>
-    )}  
-  </div>
-  )
+      {buttonLst.map((btn) => (
+        <LinkButton {...btn} />
+      ))}
+    </div>
+  );
 }
 
-export default function SidePanel({buttonLst})
-{
+export default function SidePanel({ buttonLst }) {
   const { sidePanelIsOpen } = useContext(SidePanelContext);
   return (
-      <div className={`flex-col
+    <div
+      className={`flex-col
                   bg-emerald-700
                   h-screen
                   fixed top-20
                   xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2 w-full
-                  ${sidePanelIsOpen ? "visible" : "hidden"}`}>
-          <SidePanelOptions buttonLst={buttonLst}/>
-      </div>
+                  ${sidePanelIsOpen ? "visible" : "hidden"}`}
+    >
+      <SidePanelOptions buttonLst={buttonLst} />
+    </div>
   );
 }

17067ab0a1abf203e21c9151a385f5e64f4856e7
OweT1 <owentan@hotmail.sg>
2025-02-21 01:19:58 +0800

    Added LinkButton for easier editing of Navbar

---
+++ frontend/src/components/LinkButton.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./LinkButton.css";
+
+function LinkButton(props) {
+  return (
+    <Link
+      key={props.idx}
+      to={props.webUrl}
+      className="
+      block
+      p-6
+      h-20 w-full
+      font-sans font-semibold font-size
+      text-left align-middle text-white
+      hover:bg-emerald-500
+      cursor-pointer
+      "
+    >
+      {props.webName}
+    </Link>
+  );
+}
+
+export default LinkButton;

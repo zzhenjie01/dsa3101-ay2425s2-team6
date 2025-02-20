5aead01532eb43cd7c43a494ad56d105c3bcf9f7
OweT1 <owentan@hotmail.sg>
2025-02-21 01:21:18 +0800

    Added spacing for better readability

--- frontend/src/components/chatbotToggle.jsx
+++ frontend/src/components/chatbotToggle.jsx
@@ -1,49 +1,52 @@
-import './chatbotToggle.css';
-import { useContext, useState } from 'react';
-import { ChatBotContext } from '../context/contexts';
-function ChatbotToggleButton()
-{
-    const {chatbotOpen, setChatbotOpen } = useContext(ChatBotContext);
-    return (
-        <button className='
+import "./ChatbotToggle.css";
+import { useContext, useState } from "react";
+import { ChatBotContext } from "../context/contexts";
+
+function ChatbotToggleButton() {
+  const { chatbotOpen, setChatbotOpen } = useContext(ChatBotContext);
+  return (
+    <button
+      className="
                 absolute bottom-0 right-0
                 w-27/100
                 h-10
-                border-black border-2'
-                onClick={()=>setChatbotOpen(!chatbotOpen)}>
-            Click here to open chatbot
-        </button>
-    )
+                border-black border-2"
+      onClick={() => setChatbotOpen(!chatbotOpen)}
+    >
+      Click here to open chatbot
+    </button>
+  );
 }
 
 /*
 Need to do responsive chatbot on resizing
 */
-function ChatbotPopup()
-{
-    const { chatbotOpen } = useContext(ChatBotContext);
-    return (
-        <div className={`
+function ChatbotPopup() {
+  const { chatbotOpen } = useContext(ChatBotContext);
+  return (
+    <div
+      className={`
             absolute bottom-10 right-0
             w-27/100 min-w-50 h-2/5
             border-gray border-2
             flex justify-center items-center
-            ${chatbotOpen ? "visible" : "hidden"}`}>
-            This is a popup for the chatbot
-        </div>
-    )
+            ${chatbotOpen ? "visible" : "hidden"}`}
+    >
+      This is a popup for the chatbot
+    </div>
+  );
 }
 
-export default function ChatbotDiv()
-{   
-    const [chatbotOpen, setChatbotOpen] = useState(false);
-    const chatbotOpenObj = {
-        chatbotOpen, setChatbotOpen
-    };
-    return (
-        <ChatBotContext.Provider value={chatbotOpenObj}>
-            <ChatbotToggleButton/>
-            <ChatbotPopup/>
-        </ChatBotContext.Provider>
-    )
+export default function ChatbotDiv() {
+  const [chatbotOpen, setChatbotOpen] = useState(false);
+  const chatbotOpenObj = {
+    chatbotOpen,
+    setChatbotOpen,
+  };
+  return (
+    <ChatBotContext.Provider value={chatbotOpenObj}>
+      <ChatbotToggleButton />
+      <ChatbotPopup />
+    </ChatBotContext.Provider>
+  );
 }

662cae48c86d7fc5e2f652df95380ee20fdd8296
OweT1 <owentan@hotmail.sg>
2025-02-21 01:27:58 +0800

    Created forecastPage

---
+++ frontend/src/pages/forecastPage.jsx
@@ -0,0 +1,10 @@
+import "./forecastPage.css";
+
+export default function ForecastPage() {
+  return (
+    <div className="flex-grow pt-20 text-center">
+      <h1> Welcome to the Forecasting Page </h1>
+      <p> Please make your desired predictions. </p>
+    </div>
+  );
+}

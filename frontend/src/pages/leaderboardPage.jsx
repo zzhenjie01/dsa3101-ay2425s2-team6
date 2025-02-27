256bd0a273aeb238d6291b0a4ded5bbe2e33c8fa
OweT1 <owentan@hotmail.sg>
2025-02-28 01:09:06 +0800

    updated text content

--- frontend/src/pages/leaderboardPage.jsx
+++ frontend/src/pages/leaderboardPage.jsx
@@ -1,7 +1,7 @@
 export default function LeaderboardPage() {
   return (
     <div className="flex-grow pt-20 text-center">
-      <h1>Welcome to the leaderboards!</h1>
+      <h1>Welcome to the leaderboard!</h1>
     </div>
   );
 }

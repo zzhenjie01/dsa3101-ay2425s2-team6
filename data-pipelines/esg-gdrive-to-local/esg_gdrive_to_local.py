14e982a6594157b5895ac9f6aaa17bd6fee1807e
zzhenjie01 <e0957630@u.nus.edu>
2025-03-01 12:07:23 +0800

    Updated scripts `esg_gdrive_to_local.py` and `esg_pdf_to_json.py` to create output directory if it does not exist.

--- data-pipelines/esg-gdrive-to-local/esg_gdrive_to_local.py
+++ data-pipelines/esg-gdrive-to-local/esg_gdrive_to_local.py
@@ -10,5 +10,8 @@ if __name__ == '__main__':
     SERVICE_ACCOUNT_FILE = os.getenv("SERVICE_ACCOUNT_FILE")
     SERVICE_ACCOUNT_SCOPES = [scope for scope in os.getenv("SERVICE_ACCOUNT_SCOPES").split(",") if scope != '']
     DEST_LOC = os.getenv("DEST_FOLDER")
+    # Create the destination directory if it does not exist
+    if not os.path.exists(DEST_LOC):
+        os.makedirs(DEST_LOC)
     gdrive_service = build_gdrive_svc(SERVICE_ACCOUNT_FILE, SERVICE_ACCOUNT_SCOPES)
     download_esg_reports(gdrive_service, DEST_LOC)

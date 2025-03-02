d7f8a3375896c3a7d891f83b9c81566a67370d13
OweT1 <owentan@hotmail.sg>
2025-03-02 20:38:50 +0800

    updated to pull user connection details from .env file

--- backend/createUserCredentialsCollection.js
+++ backend/createUserCredentialsCollection.js
@@ -1,27 +1,37 @@
 import mongoose from "mongoose";
-
+import "dotenv/config";
 /*
 Connect to users database with following credentials
 user: root
 password: root.
 need to move on .env file
 */
-mongoose.connect('mongodb://root:root@localhost:27017/users?authSource=admin')
-    .then(() => console.log('MongoDB connected'))
-    .catch(error => console.error('MongoDB connection error:', error));
 
+// pull details from .env file
+const user = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+
+mongoose
+  .connect(
+    `mongodb://${user}:${password}@localhost:27017/users?authSource=admin`
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.error("MongoDB connection error:", error));
+
+// define userSchema
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    }
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-const User = mongoose.model('users', userSchema);
+// Create User model
+const User = mongoose.model("users", userSchema);
 
 export default User;

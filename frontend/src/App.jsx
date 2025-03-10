207192df2240403df4186a37f9d58156be132c79
OweT1 <owentan@hotmail.sg>
2025-03-10 22:57:01 +0800

    added userContext and also Toaster extension for notifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,12 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ChatbotDiv from "./components/ChatbotToggle";
 import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
+import { Toaster } from "react-hot-toast";
+import { UserContextProvider } from "./context/contextFunction";
+
+axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.withCredentials = true;
 
 /*
 This folder provides the Outlet component for the react router.
@@ -11,10 +17,13 @@ This folder provides the Outlet component for the react router.
 export default function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes />
-      <Footer />
-      <ChatbotDiv />
+      <UserContextProvider>
+        <Header />
+        <Routes />
+        <Footer />
+        <Toaster position="bottom-left" toastOptions={{ duration: 4000 }} />
+        <ChatbotDiv />
+      </UserContextProvider>
     </BrowserRouter>
   );
 }

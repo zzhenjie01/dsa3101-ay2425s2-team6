dc967483d36d3783a8d3f10ed32596346731429b
OweT1 <owentan@hotmail.sg>
2025-03-12 19:28:37 +0800

    reset homepage to default

--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -1,13 +1,7 @@
-import { useContext } from "react";
-import { UserContext } from "@/context/context.js";
-
 export default function HomePage() {
-  const { user } = useContext(UserContext);
-
   return (
     <div className="flex-grow pt-20 text-center">
       <h1>Welcome to our Website! </h1>
-      {!!user && <h2>Hi {user.email}!</h2>}
       <p>Keep an eye out for updates in the future! :D</p>
     </div>
   );

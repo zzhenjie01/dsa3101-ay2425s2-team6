2404e8cc33afed32329887dc5c6ad9596ee66b98
OweT1 <owentan@hotmail.sg>
2025-03-12 19:26:02 +0800

    created guestprofile for guests

---
+++ backend/auth/guestProfile.js
@@ -0,0 +1,10 @@
+const guestProfile = {
+  name: "Guest",
+  email: "",
+  password: "",
+  environmentalRating: 50,
+  socialRating: 50,
+  governanceRating: 50,
+};
+
+export default guestProfile;

6df987809adaed619a6c4c85600b9221a012030d
OweT1 <owentan@hotmail.sg>
2025-03-12 19:28:22 +0800

    edited contextprovider function to account for guestprofiles

--- frontend/src/context/contextFunction.jsx
+++ frontend/src/context/contextFunction.jsx
@@ -1,15 +1,34 @@
 import { UserContext } from "./context.js";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import toast from "react-hot-toast";
+import guestProfile from "../../../backend/auth/guestProfile.js";
 
 export function UserContextProvider({ children }) {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(guestProfile);
+
   useEffect(() => {
-    if (!user) {
-      axios.get("/auth/profile").then(({ data }) => {
-        setUser(data);
-      });
-    }
+    const fetchUserProfile = async () => {
+      try {
+        const response = await axios.get("/auth/profile");
+        if (response.data === "Request Cookie not found") {
+          // Handle the case where no token is found
+          setUser(guestProfile);
+        } else {
+          setUser(response.data);
+        }
+      } catch (error) {
+        console.error("Error fetching user profile:", error);
+        // If an error occurs, keep the user as the guest profile
+      }
+    };
+    fetchUserProfile();
+
+    // if (!user) {
+    //   axios.get("/auth/profile").then(({ data }) => {
+    //     setUser(data);
+    //   });
+    // }
   }, []);
   return (
     <UserContext.Provider value={{ user, setUser }}>

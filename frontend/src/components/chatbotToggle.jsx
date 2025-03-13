270191a94055e8d417151d61c82acbeb7ceb6dae
OweT1 <owentan@hotmail.sg>
2025-03-13 20:17:54 +0800

    edited to have cursor-pointer on hover

--- frontend/src/components/chatbotToggle.jsx
+++ frontend/src/components/chatbotToggle.jsx
@@ -13,7 +13,8 @@ function ChatbotToggleButton() {
                 fixed bottom-11 right-0
                 w-27/100
                 h-10
-                border-black border-2"
+                border-black border-2
+                hover:cursor-pointer"
         onClick={() => setChatbotOpen(!chatbotOpen)}
       >
         <MessageCircle className="inline w-5 h-5 mr-2" />

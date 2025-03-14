ea5b4f507a0535b35c42a738248067b05417a294
OweT1 <owentan@hotmail.sg>
2025-03-15 00:16:06 +0800

    added logo attr for sidepanel logos

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,29 +7,29 @@ import LoginBtn from "./LoginBtn";
 import LogoutBtn from "./LogoutBtn";
 import WelcomeMsg from "./welcomeMsg.jsx";
 import WeightsButton from "./WeightsButton.jsx";
-// import ChatbotDiv from "../components/ChatbotToggle";
 import { SidePanelContext } from "../context/context.js";
 import { useLocation } from "react-router-dom";
+import { GoHome, GoGraph, GoTrophy } from "react-icons/go";
 
 const sidePanelButtonsLst = [
   {
     idx: 1,
     webUrl: "/home",
-    // imgUrl: "/assets/home.png",
+    logo: <GoHome />,
     webName: "Home",
   },
 
   {
     idx: 2,
     webUrl: "/leaderboard",
-    // imgUrl: "/assets/dashboard.png",
+    logo: <GoTrophy />,
     webName: "Leaderboard",
   },
 
   {
     idx: 3,
     webUrl: "/dashboard",
-    // imgUrl: "/assets/dashboard.png",
+    logo: <GoGraph />,
     webName: "Dashboard",
   },
 ];

4e548d43799485748e825109b2cdcb11d68ea257
OweT1 <owentan@hotmail.sg>
2025-03-20 21:08:58 +0800

    created new tables in psql and changed to Pool from client

--- backend/src/pgserver.js
+++ backend/src/pgserver.js
@@ -1,16 +1,35 @@
-import client from "./api/models/postgresDB.js";
+import pgPool from "./api/models/postgresDB.js";
 
 export const createAllTables = async () => {
   try {
-    await client.query(
+    await pgPool.query(
       `CREATE TABLE IF NOT EXISTS 
         weight_transactions(
-        user_id text NOT NULL,
+        user_id TEXT NOT NULL,
         transaction_datetime TIMESTAMP NOT NULL,
         environmental_weight INTEGER NOT NULL,
         social_weight INTEGER NOT NULL,
         governance_weight INTEGER NOT NULL
         )`
+    );
+
+    await pgPool.query(
+      `CREATE TABLE IF NOT EXISTS 
+        click_transactions(
+        user_id TEXT NOT NULL,
+        click_datetime TIMESTAMP NOT NULL,
+        company_name text NOT NULL
+        )`
+    );
+
+    await pgPool.query(
+      `CREATE TABLE IF NOT EXISTS 
+        company_stock_data(
+        company_name TEXT NOT NULL,
+        price_type TEXT NOT NULL,
+        price_date TIMESTAMP NOT NULL,
+        price FLOAT NOT NULL
+      )`
     );
 
     console.log("All tables successfully created in Postgres DB");

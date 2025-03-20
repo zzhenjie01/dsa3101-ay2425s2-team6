413b41b14b015198ed51adce72060a6eef47a96f
OweT1 <owentan@hotmail.sg>
2025-03-20 21:10:39 +0800

    export function to log weights into weights_transactions table when registering

--- backend/src/api/controllers/authHelper.js
+++ backend/src/api/controllers/authHelper.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import pgPool from "../models/postgresDB.js";
 
 export const hashPassword = (password) => {
   const saltLayers = 10;
@@ -22,7 +23,30 @@ export const comparePassword = (password, hashed) => {
   return bcrypt.compare(password, hashed);
 };
 
+export const logWeights = async (user) => {
+  await pgPool.query(
+    `
+    INSERT INTO weight_transactions
+      (user_id, 
+      transaction_datetime, 
+      environmental_weight, 
+      social_weight, 
+      governance_weight)
+    VALUES
+      ($1, NOW(), $2, $3, $4)
+    `,
+    [
+      `_${user._id.toString()}`, // value cannot start with a digit
+      user.environmentalWeight,
+      user.socialWeight,
+      user.governanceWeight,
+    ]
+  );
+  console.log("Initial User Weights logged");
+};
+
 export default {
   hashPassword,
   comparePassword,
+  logWeights,
 };

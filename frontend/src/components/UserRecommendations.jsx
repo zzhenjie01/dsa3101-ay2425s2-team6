c0d2f4de5d01a84c394ed84b75b865dab58e6110
L-Cherron <e0958318@u.nus.edu>
2025-03-22 18:41:48 +0800

    Added recommended companies tiles

--- frontend/src/components/UserRecommendations.jsx
+++ frontend/src/components/UserRecommendations.jsx
@@ -1,9 +1,34 @@
 export default function UserRecommendations({ companies }) {
-  return (
-    <ul>
-      {companies.map((company, index) => (
-        <li key={index}>{company}</li>
-      ))}
-    </ul>
-  );
+    return (
+        <ul className="flex flex-col items-center space-y-4 pb-20">
+            {companies.map((company, index) => {
+                const logoUrl = `https://raw.githubusercontent.com/icongo/bank-logos/main/logos/${company.toLowerCase().replace(/\s+/g, '-')}-rect.svg`;
+
+                return (
+                    <li
+                        key={index}
+                        className="w-[280px] h-[50px] text-[35px] px-4 py-2 bg-emerald-700 
+                                text-white font-semibold flex items-center justify-start space-x-3 
+                                rounded-lg shadow-md relative"
+                    >
+                        {/* Logo */}
+                        <img
+                            src={logoUrl}
+                            alt={`${company} logo`}
+                            className="w-12 h-12 absolute left-[50px]"
+                            onError={(e) => (e.target.src = 'https://www.svgrepo.com/download/9509/bank.svg')}
+                        />
+
+                        {/* Text */}
+                        <span
+                            className="absolute left-[60px] text-left"
+                            style={{ left: '130px' }}  // X (for logo) + Y (spacing for text)
+                        >
+                            {company}
+                        </span>
+                    </li>
+                );
+            })}
+        </ul>
+    );
 }

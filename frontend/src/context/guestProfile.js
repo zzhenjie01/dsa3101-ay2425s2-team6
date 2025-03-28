27e203d08885c845ad159291b61723599dab820f
OweT1 <owentan@hotmail.sg>
2025-03-28 22:44:16 +0800

    edited to include guestProfile configuration for simpler logic

---
+++ frontend/src/context/guestProfile.js
@@ -0,0 +1,11 @@
+export const guestProfile = {
+  _id: null,
+  name: "Guest",
+  email: "",
+  password: "",
+  environmentalWeight: 50,
+  socialWeight: 50,
+  governanceWeight: 50,
+};
+
+export default guestProfile;
